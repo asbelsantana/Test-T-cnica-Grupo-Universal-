9f93ab5c02825b33fc1f3ccb84e308332a535103
Asbel <asbelsj@outlook.com>
2025-11-25 14:17:55 -0400

    Proyecto completando

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+390ff71396820aded46276cfa9fe9fb9d5545715")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eacf3db063e1663be0db10cb31cacbe643247b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
